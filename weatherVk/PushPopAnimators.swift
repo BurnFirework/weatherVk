855fa5e73ca3606987fe5910f875125a4f4799e8
Arina Sokolova <arinylka2003@gmail.com>
2022-08-01 17:52:23 +0300

    Completed hw9

---
+++ weatherVk/PushPopAnimators.swift
@@ -0,0 +1,97 @@
+//
+//  CustomPush+PopAnimators.swift
+//  WeatherApp
+//
+//  Created by user on 31/07/2019.
+//  Copyright © 2019 Morizo Digital. All rights reserved.
+//
+
+import UIKit
+
+extension UIView {
+    func setAnchorPoint(_ point: CGPoint) {
+        var newPoint = CGPoint(x: bounds.size.width * point.x, y: bounds.size.height * point.y)
+        var oldPoint = CGPoint(x: bounds.size.width * layer.anchorPoint.x, y: bounds.size.height * layer.anchorPoint.y);
+
+        newPoint = newPoint.applying(transform)
+        oldPoint = oldPoint.applying(transform)
+
+        var position = layer.position
+
+        position.x -= oldPoint.x
+        position.x += newPoint.x
+
+        position.y -= oldPoint.y
+        position.y += newPoint.y
+
+        layer.position = position
+        layer.anchorPoint = point
+    }
+}
+
+class PushAnimator: NSObject, UIViewControllerAnimatedTransitioning {
+    private let animationDuration: TimeInterval = 0.7
+    
+    func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
+        return animationDuration
+    }
+    
+    func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+        guard let source = transitionContext.viewController(forKey: .from),
+            let destination = transitionContext.viewController(forKey: .to) else { return }
+        
+        transitionContext.containerView.addSubview(destination.view)
+        
+        source.view.frame = transitionContext.containerView.frame
+        destination.view.frame = transitionContext.containerView.frame
+        
+        destination.view.setAnchorPoint(CGPoint(x: 1, y: 0))
+        destination.view.transform = CGAffineTransform(translationX: source.view.bounds.width,
+                                                       y: 0).rotated(by: -Double.pi / 2)
+        
+        UIView.animateKeyframes(withDuration: animationDuration, delay: 0, options: .calculationModePaced, animations: {
+            UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 1, animations: {
+                destination.view.transform = CGAffineTransform(translationX: 0, y: 0).rotated(by: 0)
+            })
+        }) { finished in
+            if finished && !transitionContext.transitionWasCancelled {
+                source.view.transform = .identity
+            }
+            transitionContext.completeTransition(finished && !transitionContext.transitionWasCancelled)
+        }
+    }
+}
+
+class PopAnimator: NSObject, UIViewControllerAnimatedTransitioning {
+    private let animationDuration: TimeInterval = 1
+    
+    func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
+        return animationDuration
+    }
+    
+    func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
+        guard let source = transitionContext.viewController(forKey: .from),
+            let destination = transitionContext.viewController(forKey: .to) else { return }
+        
+        transitionContext.containerView.addSubview(destination.view)
+        transitionContext.containerView.sendSubviewToBack(destination.view)
+        
+        source.view.frame = transitionContext.containerView.frame
+        destination.view.frame = transitionContext.containerView.frame
+        
+        source.view.setAnchorPoint(CGPoint(x: 1, y: 0))
+        
+        UIView.animateKeyframes(withDuration: animationDuration, delay: 0, options: .calculationModePaced, animations: {
+            UIView.addKeyframe(withRelativeStartTime: 0, relativeDuration: 1, animations: {
+                source.view.transform = CGAffineTransform(translationX: 0, y: 0).rotated(by: -Double.pi / 2)
+            })
+        }) { finished in
+            if finished && !transitionContext.transitionWasCancelled {
+                source.removeFromParent()
+            } else if transitionContext.transitionWasCancelled {
+                destination.view.transform = .identity
+            }
+            transitionContext.completeTransition(finished && !transitionContext.transitionWasCancelled)
+        }
+    }
+}
